f4b9c21197f784d92b70e75799fb02e3436e1412
AIex05 <alex.yt.zhu@gmail.com>
2022-02-04 15:31:10 -0500

    Data generation

--- DataGeneration.py
+++ DataGeneration.py
@@ -1,3 +1,11 @@
-import numpy as np;
-import math;
+import random
 
+if __name__ == '__main__':
+    num_points = 1000
+    f = open(r"C:\Users\Alex\IdeaProjects\CS4433_Project_2\data_points.txt", "a")
+    for i in range(num_points):
+        x = random.randint(0,10000)
+        y = random.randint(0,10000)
+        point = str(x) + "," + str(y) + "\n"
+        f.write(point)
+    f.close()
