65263ba49e89995071a9e7fc41789a4cf790cf9c
Lauren Hatfield <lhatfield15@gmail.com>
2022-02-08 20:28:01 -0500

    Part 1B works

---
+++ src/test/java/FileSystemBase.java
@@ -0,0 +1,3 @@
+public class FileSystemBase {
+    public static String fileBase = "C:/Users/lhatf/Documents/College/CS4433/VM_shared_folder/cs4433_project1/cs4433-project2";
+}
--- src/test/java/KmeanClustering$Test.java
+++ src/test/java/KmeanClustering$Test.java
@@ -1,13 +1,16 @@
+import org.apache.commons.io.FileUtils;
 import org.junit.Test;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Scanner;
 
 import static org.junit.Assert.*;
 
 public class KmeanClustering$Test {
 
+    public String outputFolder = "/KMeansOutput";
     @Test
     public void debug() throws Exception {
         String[] input = new String[4];
@@ -19,39 +22,50 @@ public class KmeanClustering$Test {
             mac or linux: update the path like "file:///Users/.../projectDirectory/data.txt"
         */
 
-        System.setProperty("hadoop.home.dir", "C:\\winutils\\");
-        input[0] = "file:///C:/Users/Alex/IdeaProjects/CS4433_Project_2/data_points.txt";
-        input[1] = "file:///C:/Users/Alex/IdeaProjects/CS4433_Project_2/Test_results/output.txt";
+//        System.setProperty("hadoop.home.dir", "C:\\winutils\\");
+        input[0] = "file:///" + FileSystemBase.fileBase + "/data_points.txt";
+        input[1] = "file:///" + FileSystemBase.fileBase + outputFolder;
         input[2] = "4904,121 5627.009,657.0088";
         input[3] = "5";
 
+        boolean reset = false;
 
-        int R = Integer.valueOf(input[3]);
-        for (int i = 0; i < R; i++){
+        if (reset) {
+            //run to reset
             KmeanClustering wc = new KmeanClustering();
             wc.debug(input);
-
-            //read from output path -- https://www.w3schools.com/java/java_files_read.asp
-            String centroids = "";
-            try {
-                File myObj = new File(input[1] + "/" + "part-r-00000");
-                Scanner myReader = new Scanner(myObj);
-                while (myReader.hasNextLine()) {
-                    String data[] = myReader.nextLine().split("\t");
-                    centroids += " " + data[0] + "," + data[1];
+        } else {
+            int R = Integer.valueOf(input[3]);
+            for (int i = 0; i < R; i++) {
+                //read from output path -- https://www.w3schools.com/java/java_files_read.asp
+                String centroids = "";
+                try {
+                    File myObj = new File(FileSystemBase.fileBase + outputFolder + "/part-r-00000");
+                    Scanner myReader = new Scanner(myObj);
+                    while (myReader.hasNextLine()) {
+                        String data[] = myReader.nextLine().split("\t");
+                        centroids += " " + data[0] + "," + data[1];
+                    }
+                    //assume centroids will never be 0 chars
+                    input[2] = centroids.substring(1);
+                    myReader.close();
+                } catch (FileNotFoundException e) {
+                    System.out.println("Error finding file to read");
                 }
-                //assume centroids will never be 0 chars
-                input[2] = centroids.substring(1);
-                myReader.close();
-            } catch (FileNotFoundException e) {
-                System.out.println("An error occurred reading the output file.");
-            }
 
-            //delete output path -- https://www.w3schools.com/java/java_files_delete.asp
-            File myObj = new File(input[1]);
-            if (!myObj.delete()) {
-                System.out.println("Failed to delete the output file.");
+                try {
+                    FileUtils.deleteDirectory(new File(FileSystemBase.fileBase + outputFolder));
+                }
+                catch (IOException e){
+                    System.out.println("deletion is unsuccessful");
+                }
+                catch (IllegalArgumentException e){
+                    System.out.println("directory does not exist or is not a directory");
+                }
+
+                KmeanClustering wc = new KmeanClustering();
+                wc.debug(input);
             }
-        }
+        }//end reset else
     }
 }
