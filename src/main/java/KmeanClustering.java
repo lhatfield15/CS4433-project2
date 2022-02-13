5b75f7531e9255060cabad2f21d03996436627b9
Lauren Hatfield <lhatfield15@gmail.com>
2022-02-13 13:23:02 -0500

    Main works like debug now

--- src/main/java/KmeanClustering.java
+++ src/main/java/KmeanClustering.java
@@ -1,11 +1,13 @@
-import java.io.IOException;
+import java.io.*;
 import java.lang.Math;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Random;
+import java.util.Scanner;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
@@ -15,13 +17,34 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class KmeanClustering {
 
-    public static class Point {
+    static class Point implements Comparable<Point> {
+
         float x;
         float y;
 
         public Point(float pt_x, float pt_y) {
             this.x = pt_x;
             this.y = pt_y;
+        }
+
+        public float distance(Point pt) {
+            float x_difference = this.x - pt.x;
+            float y_difference = this.y - pt.y;
+            float x2y2 = (x_difference * x_difference) + (y_difference * y_difference);
+            return (float) Math.sqrt(x2y2);
+        }
+
+        @Override
+        public int compareTo(Point pt) {
+            if (this.x > pt.x) {
+                return 1; // this is bigger
+            } else if (this.x == pt.x && this.y > pt.y) {
+                return 1; // this is bigger
+            } else if (this.x == pt.x && this.y == pt.y) {
+                return 0; // this and object are same
+            } else {
+                return -1; // this is smaller
+            }
         }
 
         public String toString() {
@@ -108,7 +131,7 @@ public class KmeanClustering {
                 points.add(data[0] + "," + data[1]);
                 float x = Float.valueOf(data[0]);
                 float y = Float.valueOf(data[1]);
-                float c = Float.valueOf(data[1]);
+                float c = Float.valueOf(data[2]);
                 x_total += x;
                 y_total += y;
                 count += c;
@@ -130,41 +153,240 @@ public class KmeanClustering {
         }
     }
 
-    public void debug(String[] args) throws Exception {
+    public static void runKMeansCycle(String[] input) throws Exception {
+        // so we don't get the error if someone forgot to delete
+        deleteOldOutputDirectory();
+        // run cycle
+
         // initiliaze job
         Configuration conf = new Configuration();
-        conf.set("centroids", args[2]);
-        conf.set("outputStyle", args[4]);
+        conf.set("centroids", input[2]);
+        conf.set("outputStyle", input[4]);
         Job job = Job.getInstance(conf, "K means");
         job.setJarByClass(KmeanClustering.class);
-        job.setMapperClass(ClosestCentroidMapper.class);
         job.setCombinerClass(CentroidRecalculatorCombiner.class);
+        job.setMapperClass(ClosestCentroidMapper.class);
         job.setReducerClass(CentroidRecalculatorReducer.class);
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(Text.class);
-        FileInputFormat.addInputPath(job, new Path(args[0]));
-        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        FileInputFormat.addInputPath(job, new Path(input[0]));
+        FileOutputFormat.setOutputPath(job, new Path(input[1]));
 
         // run job
         job.waitForCompletion(true);
     }
 
-    public static void main(String[] args) throws Exception {
-        // initiliaze job
-        Configuration conf = new Configuration();
-        conf.set("centroids", args[2]);
-        conf.set("outputStyle", args[4]);
-        Job job = Job.getInstance(conf, "K means");
-        job.setJarByClass(KmeanClustering.class);
-        job.setMapperClass(ClosestCentroidMapper.class);
-        job.setCombinerClass(CentroidRecalculatorCombiner.class);
-        job.setReducerClass(CentroidRecalculatorReducer.class);
-        job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(Text.class);
-        FileInputFormat.addInputPath(job, new Path(args[0]));
-        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+    public static ArrayList<Point> generateCentroidsFromFile() {
+        // make a list of the new centroids
+        ArrayList<Point> centroids = new ArrayList<Point>();
+        // read from output path -- https://www.w3schools.com/java/java_files_read.asp
 
-        // run job
-        job.waitForCompletion(true);
+        try {
+            // read the new centroids
+            File myObj = new File(FileSystemBase.fileBase + outputFolder + outputFile);
+            Scanner myReader = new Scanner(myObj);
+            // read each line of points
+            while (myReader.hasNextLine()) {
+                // create point from coords and add to centroid list
+                String data[] = myReader.nextLine().split("\t");
+                if (data[0].equals("Centroid")) {
+                    String coords[] = data[1].split(",");
+                    centroids.add(new Point(Float.valueOf(coords[0]), Float.valueOf(coords[1])));
+                }
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Error reading file");
+        }
+
+        // order the points in the centroids, so that each centroid is compared
+        // to the same one as last time
+        Collections.sort(centroids);
+        return centroids;
+    }
+
+    public static ArrayList<Point> generateCentroidsFromInput(String centroids_string) {
+        // get a list of the previously input centroids
+        ArrayList<Point> centroids = new ArrayList<>();
+        String[] points = centroids_string.split(" ");// get centroids
+        // put old centroids into list as Points
+        for (String pt : points) {
+            String[] coords = pt.split(",");
+            centroids.add(new Point(Float.valueOf(coords[0]), Float.valueOf(coords[1])));
+        }
+
+        return centroids;
+    }
+
+    public static boolean hasConverged(ArrayList<Point> new_centroids, ArrayList<Point> old_centroids,
+            float convergence_distance_threshold) {
+        // ensure the number of centroids has stayed the same since last iteration
+        if (old_centroids.size() != new_centroids.size()) {
+            if (new_centroids.size() > old_centroids.size()) {
+                System.out.println("Problem, new centroids created");
+                System.exit(1);
+            } else {
+                // generate a new point for a centroid
+                Random rand = new Random();
+                while (old_centroids.size() > new_centroids.size()) {
+                    float x = rand.nextFloat() * 10000;
+                    float y = rand.nextFloat() * 10000;
+                    new_centroids.add(new Point(x, y));
+                }
+            }
+        }
+
+        // compare if the old centroids were within tolerance of new centroids
+        for (int j = 0; j < old_centroids.size(); j++) {
+            if (old_centroids.get(j).distance(new_centroids.get(j)) > convergence_distance_threshold) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void deleteOldOutputDirectory() {
+        // delete the old output directory
+        try {
+            FileUtils.deleteDirectory(new File(FileSystemBase.fileBase + outputFolder));
+        } catch (IOException e) {
+            System.out.println("deletion is unsuccessful");
+        } catch (IllegalArgumentException e) {
+            System.out.println("directory does not exist or is not a directory");
+        }
+    }
+
+    public static String newInputFromCentroids(ArrayList<Point> centroids) {
+        // convert the new centroids into a form for passing to mapreduce
+        String new_input = "";
+        if (centroids.size() == 0) {
+            System.out.println("There were no centroids. This is an error");
+        }
+        for (Point p : centroids) {
+            new_input += " " + String.valueOf(p.x) + "," + String.valueOf(p.y);
+        }
+        return new_input.substring(1);
+    }
+
+    public static void appendConvergence(boolean hasConverged) {
+        // Open given file in append mode by creating an
+        // object of BufferedWriter class
+        try {
+            BufferedWriter out = new BufferedWriter(
+                    new FileWriter(FileSystemBase.fileBase + outputFolder + outputFile, true));
+
+            // Writing on output stream
+            if (hasConverged) {
+                out.write("The System Converged.");
+            } else {
+                out.write("The System Did Not Converge.");
+            }
+            // Closing the connection
+            out.close();
+        } catch (Exception e) {
+            System.out.println("Error writing convergence to file");
+        }
+    }
+
+    public static String readCentroidSeeds() {
+        String centroids = "";// form of x,y x1,y1 x2,y2...
+        try {
+            // read the new centroids
+            File myObj = new File(FileSystemBase.fileBase + "/seed_points.txt");
+            Scanner myReader = new Scanner(myObj);
+            // read each line of points
+            while (myReader.hasNextLine()) {
+                // create point from coords and add to centroid list
+                centroids += " " + myReader.nextLine();
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Couldn't find seed file.");
+        }
+        return centroids.substring(1);
+    }
+
+    public static String outputFile = "/part-r-00000";
+    public static String outputFolder = "/KMeansOutput";
+
+    public void debug(String[] args) throws Exception {
+        args[2] = readCentroidSeeds();
+
+        float convergence_distance_threshold = 1;
+        boolean hasConverged = false;
+
+        int R = Integer.valueOf(args[3]);
+        for (int i = 0; i < R; i++) {
+            // check for convergence only if not first cycle
+            if (i > 0) {
+                // acquire past (old) and present (new) centroids
+                ArrayList<Point> new_centroids = generateCentroidsFromFile();
+                ArrayList<Point> old_centroids = generateCentroidsFromInput(args[2]);
+
+                // end program if centroids are in convergence
+                if (hasConverged(new_centroids, old_centroids, convergence_distance_threshold)) {
+                    hasConverged = true;
+                    break;
+                }
+
+                args[2] = newInputFromCentroids(new_centroids);
+            }
+
+            // Run one iteration of kmeans clustering
+             runKMeansCycle(args);
+        } // end cycle for loop
+        if (args[4].equals("a")) {
+            appendConvergence(hasConverged);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        args[2] = readCentroidSeeds();
+
+        float convergence_distance_threshold = 1;
+        boolean hasConverged = false;
+
+        int R = Integer.valueOf(args[3]);
+        for (int i = 0; i < R; i++) {
+            // check for convergence only if not first cycle
+            if (i > 0) {
+                // acquire past (old) and present (new) centroids
+                ArrayList<Point> new_centroids = generateCentroidsFromFile();
+                ArrayList<Point> old_centroids = generateCentroidsFromInput(args[2]);
+
+                // end program if centroids are in convergence
+                if (hasConverged(new_centroids, old_centroids, convergence_distance_threshold)) {
+                    hasConverged = true;
+                    break;
+                }
+
+                args[2] = newInputFromCentroids(new_centroids);
+            }
+
+            // Run one iteration of kmeans clustering
+            // runKMeansCycle(args);
+            deleteOldOutputDirectory();
+            // run cycle
+
+            // initiliaze job
+            Configuration conf = new Configuration();
+            conf.set("centroids", args[2]);
+            conf.set("outputStyle", args[4]);
+            Job job = Job.getInstance(conf, "K means");
+            job.setJarByClass(KmeanClustering.class);
+            job.setCombinerClass(CentroidRecalculatorCombiner.class);
+            job.setMapperClass(ClosestCentroidMapper.class);
+            job.setReducerClass(CentroidRecalculatorReducer.class);
+            job.setOutputKeyClass(Text.class);
+            job.setOutputValueClass(Text.class);
+            FileInputFormat.addInputPath(job, new Path(args[0]));
+            FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+            // run job
+            job.waitForCompletion(true);
+        } // end cycle for loop
+        if (args[4].equals("a")) {
+            appendConvergence(hasConverged);
+        }
     }
 }
--- src/test/java/KmeanClusteringTest.java
+++ src/test/java/KmeanClusteringTest.java
@@ -8,168 +8,10 @@ import static org.junit.Assert.*;
 
 public class KmeanClusteringTest {
 
-    class Point implements Comparable<Point> {
-
-        float x;
-        float y;
-
-        public Point(float pt_x, float pt_y) {
-            this.x = pt_x;
-            this.y = pt_y;
-        }
-
-        public float distance(Point pt) {
-            float x_difference = this.x - pt.x;
-            float y_difference = this.y - pt.y;
-            float x2y2 = (x_difference * x_difference) + (y_difference * y_difference);
-            return (float) Math.sqrt(x2y2);
-        }
-
-        @Override
-        public int compareTo(Point pt) {
-            if (this.x > pt.x) {
-                return 1; // this is bigger
-            } else if (this.x == pt.x && this.y > pt.y) {
-                return 1; // this is bigger
-            } else if (this.x == pt.x && this.y == pt.y) {
-                return 0; // this and object are same
-            } else {
-                return -1; // this is smaller
-            }
-        }
-    }
-
-    public void runKMeansCycle(String[] input) throws Exception {
-        // so we don't get the error if someone forgot to delete
-        deleteOldOutputDirectory();
-        //run cycle
-        KmeanClustering wc = new KmeanClustering();
-        wc.debug(input);
-    }
-
-    public ArrayList<Point> generateCentroidsFromFile() {
-        // make a list of the new centroids
-        ArrayList<Point> centroids = new ArrayList<Point>();
-        // read from output path -- https://www.w3schools.com/java/java_files_read.asp
-
-        try {
-            // read the new centroids
-            File myObj = new File(FileSystemBase.fileBase + outputFolder + outputFile);
-            Scanner myReader = new Scanner(myObj);
-            // read each line of points
-            while (myReader.hasNextLine()) {
-                // create point from coords and add to centroid list
-                String data[] = myReader.nextLine().split("\t");
-                if (data[0].equals("Centroid")) {
-                    String coords[] = data[1].split(",");
-                    centroids.add(new Point(Float.valueOf(coords[0]), Float.valueOf(coords[1])));
-                }
-            }
-            myReader.close();
-        } catch (FileNotFoundException e) {
-            System.out.println("Error reading file");
-        }
-
-        // order the points in the centroids, so that each centroid is compared
-        // to the same one as last time
-        Collections.sort(centroids);
-        return centroids;
-    }
-
-    public ArrayList<Point> generateCentroidsFromInput(String centroids_string) {
-        // get a list of the previously input centroids
-        ArrayList<Point> centroids = new ArrayList<>();
-        String[] points = centroids_string.split(" ");// get centroids
-        // put old centroids into list as Points
-        for (String pt : points) {
-            String[] coords = pt.split(",");
-            centroids.add(new Point(Float.valueOf(coords[0]), Float.valueOf(coords[1])));
-        }
-
-        return centroids;
-    }
-
-
-    public boolean hasConverged(ArrayList<Point> new_centroids, ArrayList<Point> old_centroids,
-            float convergence_distance_threshold) {
-        // ensure the number of centroids has stayed the same since last iteration
-        if (old_centroids.size() != new_centroids.size()) {
-            System.out.println("New and Old Centroid count differed, generating new centroids");
-            if (new_centroids.size() > old_centroids.size()){
-                System.out.println("Problem, new centroids created");
-                System.exit(1);
-            }
-            else{
-                //generate a new point for a centroid
-                Random rand = new Random();
-                while(old_centroids.size() > new_centroids.size()){
-                    float x = rand.nextFloat() * 10000;
-                    float y = rand.nextFloat() * 10000;
-                    new_centroids.add(new Point(x, y));
-                }
-            }
-        }
-
-        // compare if the old centroids were within tolerance of new centroids
-        for (int j = 0; j < old_centroids.size(); j++) {
-            if (old_centroids.get(j).distance(new_centroids.get(j)) > convergence_distance_threshold) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    public void deleteOldOutputDirectory() {
-        // delete the old output directory
-        try {
-            FileUtils.deleteDirectory(new File(FileSystemBase.fileBase + outputFolder));
-        } catch (IOException e) {
-            System.out.println("deletion is unsuccessful");
-        } catch (IllegalArgumentException e) {
-            System.out.println("directory does not exist or is not a directory");
-        }
-    }
-
-    public String newInputFromCentroids(ArrayList<Point> centroids) {
-        // convert the new centroids into a form for passing to mapreduce
-        String new_input = "";
-        if (centroids.size() == 0) {
-            System.out.println("There were no centroids. This is an error");
-        }
-        for (Point p : centroids) {
-            new_input += " " + String.valueOf(p.x) + "," + String.valueOf(p.y);
-        }
-        return new_input.substring(1);
-    }
-
-    public void appendConvergence(boolean hasConverged){
-        // Open given file in append mode by creating an
-        // object of BufferedWriter class
-        try {
-            BufferedWriter out = new BufferedWriter(
-                    new FileWriter(FileSystemBase.fileBase + outputFolder + outputFile, true));
-
-            // Writing on output stream
-            if (hasConverged){
-                out.write("The System Converged.");
-            }
-            else{
-                out.write("The System Did Not Converge.");
-            }
-            // Closing the connection
-            out.close();
-        }
-        catch (Exception e){
-            System.out.println("Error writing convergence to file");
-        }
-    }
-
-    public String outputFolder = "/KMeansOutput";
-    public String outputFile = "/part-r-00000";
 
     @Test
     public void debug() throws Exception {
-        String[] input = new String[5];
+        String[] input = new String[6];
 
         /*
          * 1. put the data.txt into a folder in your pc
@@ -183,36 +25,14 @@ public class KmeanClusteringTest {
         // This System property thing was breaking things
         // System.setProperty("hadoop.home.dir", "C:\\winutils\\");
         input[0] = "file:///" + FileSystemBase.fileBase + "/data_points.txt"; // input
-        input[1] = "file:///" + FileSystemBase.fileBase + outputFolder; // output
-        input[2] = "9109,250 250,5260"; // initial centroids
+        input[1] = "file:///" + FileSystemBase.fileBase + KmeanClustering.outputFolder; // output
+        input[2] = "";//will be filled by program later as centroid seed
         input[3] = "10"; // number of iterations
         input[4] = "a"; // output centroids/convergence = a, output clustered data points = 'b'
+        input[5] = "1"; //convergence_distance_threshold
 
-        float convergence_distance_threshold = 1;
-        boolean hasConverged = false;
+        KmeanClustering km = new KmeanClustering();
+        km.debug(input);
 
-        int R = Integer.valueOf(input[3]);
-        for (int i = 0; i < R; i++) {
-            // check for convergence only if not first cycle
-            if (i > 0) {
-                // acquire past (old) and present (new) centroids
-                ArrayList<Point> new_centroids = generateCentroidsFromFile();
-                ArrayList<Point> old_centroids = generateCentroidsFromInput(input[2]);
-
-                // end program if centroids are in convergence
-                if (hasConverged(new_centroids, old_centroids, convergence_distance_threshold)) {
-                    hasConverged = true;
-                    break;
-                }
-
-                input[2] = newInputFromCentroids(new_centroids);
-            }
-
-            // Run one iteration of kmeans clustering
-            runKMeansCycle(input);
-        }//end cycle for loop
-        if(input[4].equals("a")) {
-            appendConvergence(hasConverged);
-        }
     }//end debug
 }
